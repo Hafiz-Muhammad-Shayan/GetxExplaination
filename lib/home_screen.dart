a801d67da34d199bd955b78906e43d0a1564abda
Hafiz Muhammad Shayan <hafizshayan284@gmail.com>
2026-09-16 18:27:48 +0500

    video 8 done state management

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:getxexplaination/counter_controller.dart';
 import 'package:getxexplaination/screen_one.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,52 +12,35 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final CounterController controller = Get.put(CounterController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Center(child: Text(
+        title: Center(
+          child: Text(
             "Getx Tutorials",
           style: TextStyle(
             color: Colors.white,
           ),
         ),),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-
-        children: [
-          ListTile(
-            title: Text("message".tr),
-            subtitle: Text("name".tr),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Row(
-            children: [
-              OutlinedButton(
-                  onPressed: (){
-                    Get.updateLocale(Locale("en","US"));
-                  },
-                  child: Text('English')),
-              SizedBox(
-                width: 20,
-              ),
-              OutlinedButton(
-                  onPressed: (){
-                    Get.updateLocale(Locale("ur","PK"));
-                  },
-                  child: Text('Urdu')),
-
-            ],
-          )
-        ],
+      body: Center(
+        child: Obx((){
+          return Text(
+            controller.counter.toString(),
+            style: TextStyle(
+              fontSize: 60,
+            ),);
+        }),
       ),
 
-
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+          controller.incrementCounter();
+          }),
     );
   }
 }
